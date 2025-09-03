ba2056ca5a28882919ef1089ebc26086ac4cc85f
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-09-03 15:47:03 +0900

    feature: clothdetail 구현

--- src/app/clothdetail/page.tsx
+++ src/app/clothdetail/page.tsx
@@ -1,9 +1,7 @@
+// app/clothdetail/page.tsx
 import React from "react";
-import ClothDetailForm from "@/features/clothdetail/ClothDetailForm";
+import ClothDetailForm from "@/features/clothdetail/ClothDetailForm"
 
-/*
-/clothdetail 경로 렌더링.
-*/
 export default function ClothDetailPage() {
   return <ClothDetailForm />;
 }
